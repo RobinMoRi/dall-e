c220339e3a1465c9cc43c7a755dd455a15de9980
Robin Moreno Rinding <romori@mbpsomthorrobin.home>
2022-11-10 21:45:31 +0100

    added functionality with open ai

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,29 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Configuration, OpenAIApi } from "openai";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [search, setSearch] = useState();
+  const [image, setImage ] = useState();
+
+  const configuration = new Configuration({
+    apiKey: import.meta.env.VITE_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const generateImage = async() => {
+    const res = await openai.createImage({ prompt: search, n: 1, size: "512x512", });
+    setImage(res.data.data[0].url);
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {image ? <img src={image} className="Image"/> : undefined}
+      <textarea onChange={(e) => setSearch(e.target.value)} className="TextArea"/>
+      <button onClick={generateImage} className="Button">Generate Image</button>
     </div>
   )
 }
